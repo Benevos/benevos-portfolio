38c39a7a097cc6174afaf478f2232af695e0a415
benevos <b4dshot_assassin@hotmail.com>
2024-01-26 07:59:17 -0600

    feature: added more contact info on resumey

--- src/components/Resume/ReHeader.tsx
+++ src/components/Resume/ReHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { IoMail } from "react-icons/io5";
-import { FaPhone } from "react-icons/fa6";
+import { FaGithub, FaLinkedin, FaPhone } from "react-icons/fa6";
 import { PiCursorFill } from "react-icons/pi";
 import { MdLocationOn } from "react-icons/md";
 import { LuLanguages } from "react-icons/lu";
@@ -21,9 +21,11 @@ function ReHeader() {
                 <ReContactItem Icon={LuLanguages} title='English (Proficient), Spanish (Native)'/>
             </section>
 
-            <section className='flex flex-col gap-[2px] max-md:mt-3'>
+            <section className='flex flex-col gap-[0px] leading-[16px] max-md:mt-3'>
                 <ReContactItem Icon={IoMail} title='kevin_mendoza092@hotmail.com'/>
                 <ReContactItem Icon={FaPhone} title='+52 831 102 4666'/>
+                <ReContactItem Icon={FaGithub} title='\benevos'/>
+                <ReContactItem Icon={FaLinkedin} title='\in\benevos'/>
                 <ReContactItem Icon={PiCursorFill} title='https://benevos.vercel.app/'/>
                 <ReContactItem Icon={MdLocationOn} title='Tamaulipas, México'/>
             </section>
--- src/components/Resume/Summary/ReSummary.tsx
+++ src/components/Resume/Summary/ReSummary.tsx
@@ -4,7 +4,7 @@ import ReSectionTitle from '@/components/Resume/ReSectionTitle';
 function ReSummary() 
 {
     return (
-        <section className='w-full mt-6'>
+        <section className='w-full mt-5'>
             <ReSectionTitle title='SUMMARY'/>
             
             <p className='text-[14px] text-justify leading-4'>{"Computer science engineer with experience in fullstack solutions on projects that involve web, mobile, servers and process automation technologies. I am a self-confident and like to expose ideas while learning from others without fear of public speaking or defending postures, competent in fields such as speech, leadership and interpersonal ties. Despite all this, I am aware of not having world's knowledge, but as engineer, I believe not knowing something doesn't mean it can't be learned. My favorite hobbies are video games, gym, watching series and learning new technologies and programming methods."}</p>
